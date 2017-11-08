53bcdbebb6ddddb20a34e23f3b35a9b8e675502b
clp <clp@qq.com>
2017-11-08 21:06:39 +0800

    登录模块完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,6 +1,6 @@
 $(function(){
 	var $form = $('form');
-	console.log($form);
+	// console.log($form);
 
 	$form.bootstrapValidator({
 
@@ -11,14 +11,17 @@ $(function(){
 	    },
 	    //字段检验
 	    fields:{
-	    	userName:{
+	    	username:{
 	    		validators:{
 	    			notEmpty:{
                		 message:'用户名不能为空'
            			},
+           			callback:{
+           			 message:'用户名有误'
+           			}
 	    		}
 	    	},
-		    passWord:{
+		    password:{
 		    	validators:{
 		    		notEmpty:{
 		    			message:'密码不能为空'
@@ -27,10 +30,48 @@ $(function(){
 		    			min:6,
 		    			max:12,
 		    			message:'密码长度不符合'
+		    		},
+		    		callback:{
+		    			message:'密码有误'
 		    		}
 		    	}
 		    }
 		}
 
 	})
+	$form.on('success.form.bv',function(e){
+		console.log($(this));
+		//阻止submit的默认事件
+		e.preventDefault();
+		// console.log($form.serialize());
+		var $data =$form.serialize();
+		$.ajax({
+			url:'/employee/employeeLogin',
+			type:'post',
+			data:$data,
+			success:function(data){
+				// console.log(data);
+				// 创建实例
+				var validator = $form.data('bootstrapValidator');
+				console.log(validator);
+
+				if(data.error===1000){
+					console.log("ddsdsd");
+					//更新字段
+					validator.updateStatus('username','INVALID','callback');
+				}
+				if(data.error===1001){
+					// console.log("ddsdsd");
+					//更新字段
+					validator.updateStatus('password','INVALID','callback');
+				}
+				if(data.sueccss){
+					loadction.href = "index.html";
+				}
+			}
+		})
+		
+	})
+
+
 })
